02c2de860df2744bc81c5f0bb4d2ecf11c4fc3eb
viileppi <vili.rasanen@gmail.com>
2022-04-28 10:45:28 +0300

    first functional system

--- boot.py
+++ boot.py
@@ -4,27 +4,30 @@ import time
 
 class touchPin:
     """ a capacitive touch circuit """
-    def __init__(self, pinIn, pinOut, timeOut, id):
-        """ takes two Pin objects as input  and output,
-            sends a pulse of approximately half the timeout
-            from pinOut to pinIn. if pulse is gone before
+    def __init__(self, pin, timeOut):
+        """ takes a Pin number as input,
+            sends a pulse from pin to gnd. if pulse is 
+            gone before
             timeout, returns id (which should be int>0)
             else returns -1 or -2
             timeOut is in microseconds 
             initial timeout is 1000000us """
-        self.touchRead = pinIn
-        self.sendPulse = pinOut
+        self.id = pin
+        self.pin = Pin(self.id, Pin.OUT)
+        self.pin.off()
         self.timeOut = timeOut
-        self.sendPulse.off()
         self.pulse_level = 1    # 1 for reading high pulse, 0 for low
-        self.timer = machine.Timer()
-        self.id = id
 
     def checkPin(self):
-        self.sendPulse.on()
-        ptime = machine.time_pulse_us(self.touchRead, self.pulse_level, self.timeOut)
-        self.sendPulse.off()
-        if ptime < 0:
-            return ptime
-        else:
-            return self.id
+        self.pin.off()
+        self.pin.on()
+        self.pin = Pin(self.id, Pin.IN) 
+        ptime = machine.time_pulse_us(self.pin, self.pulse_level, self.timeOut)
+        self.pin = Pin(self.id, Pin.OUT)
+        self.pin.off()
+        return ptime
+        # if ptime < 0:
+        #     return ptime
+        # else:
+        #     return self.id
+
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import machine
 from machine import Pin
 import time
 
+
 TIMEOUT = 10000
 #pulse read pins are on GP26, GP22, gp20 and GP18.
 # touch out is on pin GP16 (1M resistor from pulse send pin)
-outPin = Pin(16, Pin.OUT)
-p0 = touchPin(Pin(26, Pin.IN), outPin, TIMEOUT, 1) 
-p1 = touchPin(Pin(22, Pin.IN), outPin, TIMEOUT, 2) 
-p2 = touchPin(Pin(20, Pin.IN), outPin, TIMEOUT, 3) 
-p3 = touchPin(Pin(18, Pin.IN), outPin, TIMEOUT, 4) 
+# outPin = Pin(16, Pin.OUT)
+oneTick = 100
+led = Pin(25, Pin.OUT)
+led.off()
+p0 = touchPin(26, TIMEOUT) 
+p1 = touchPin(22, TIMEOUT) 
+p2 = touchPin(20, TIMEOUT) 
+p3 = touchPin(18, TIMEOUT) 
 pinses = [p0, p1, p2, p3]
 
 def reportStatus(l):
@@ -17,17 +21,27 @@ def reportStatus(l):
     m = []
     for p in l:
         n = p.checkPin() 
-        if n>0:
-            m.append(n)
-        time.sleep_ms(10)
+        m.append(n)
+        # time.sleep_ms(10)
     return m
-            
 
+def blinkLed(pin, timeout):
+    pin.on()
+    time.sleep_ms(timeout)
+    pin.off()
+    time.sleep_ms(timeout)
+            
 while True:
     l = reportStatus(pinses)
-    if (l != []):
-        for p in l:
-            print(p)
+    if (l[0]>0):
+        blinkLed(led, oneTick)
+    elif (l[3]>0):
+        blinkLed(led, oneTick*2)
     else:
-        pass
-    time.sleep_ms(25)
+        time.sleep_ms(oneTick)
+    print(l)
+    # if (l != []):
+    #     for p in l:
+    #         print(p)
+    # else:
+    #     pass
